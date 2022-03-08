5c2d51f95e40a9a6ca3f8dad800987b556adfefd
Chris Mason <cmmason@crowdstryk.com>
2022-03-07 23:13:01 -0800

    Cleanup simple carmichaels implementation

--- simple_carmichaels.py
+++ simple_carmichaels.py
@@ -2,11 +2,26 @@
 
 import time
 
-def check(coprimes, m, n):
+def main():
+    value = input('Enter value at which to determine the Carmichael function: ')
+    start_time = time.time()
+   
+    try:
+        carmichaels = find_carmichaels(int(value))
+    except ValueError:
+        print('Error: Please provide an integer value')
+        raise SystemExit
+
+    print(f'\u03bb({value}) = {carmichaels}')
+    print(f'{time.time() - start_time} seconds elapsed')
+
+
+def is_valid(coprimes, m, n):
     for coprime in coprimes:
         if ((coprime**m) % n) != 1:
             return False
     return True
+
 
 def is_coprime(x, y):
     for i in range(2, max(x,y)):
@@ -15,27 +30,26 @@ def is_coprime(x, y):
 
     return True;
 
+
 def find_carmichaels(n):
+    m = 1
     coprimes = []
 
     for i in range(1, n):
         if is_coprime(i, n):
             coprimes.append(i)
-            
-    m = 1
+
     while True:
         for coprime in coprimes:
             while ((coprime**m) % n) != 1:
                 m += 1
         
-        if check(coprimes, m, n):
+        if is_valid(coprimes, m, n):
             return m
 
     return -1
 
-start_time = time.time()
 
-v = 100000
-carmichaels = find_carmichaels(v)
-print(f'Carmichaels function of {v} is: {carmichaels}')
-print("--- %s seconds ---" % (time.time() - start_time))
+if __name__ == '__main__':
+    main()
+
